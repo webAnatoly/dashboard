a58ad805cf152ad3c16b7ae4401607146bb492a6
Tolik Dem <tolik.demianov@gmail.com>
2021-02-07 20:01:29 +0300

    1) импортировал модуль "DxDashboardControlModule"
    2) После импортирования модуля "DxDashboardControlModule" при компиляции возникала ошибка вида "Property 'X' in type 'Y' is not assignable to the same property in base type 'Z'.".
    От этой ошибки я избавился путём добавления в tsconfig.json "skipLibCheck": true, теперь при компиляции он не будет проверять сторонние библиотеки.
    
    Подробнее об этой ошибке и вариантах её решения по ссылке:
    https://supportcenter.devexpress.com/ticket/details/t819718/property-x-in-type-y-is-not-assignable-to-the-same-property-in-base-type-z-error-occurs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { DxButtonModule, DxDataGridModule } from 'devextreme-angular';
+import { DxDashboardControlModule } from 'devexpress-dashboard-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     DxButtonModule,
     DxDataGridModule,
+    DxDashboardControlModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
