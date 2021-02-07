6ee020ceb5ea195949da1f7b3bf87a90edb210a9
Tolik Dem <tolik.demianov@gmail.com>
2021-02-07 15:54:41 +0300

    В шаблоне app.component.html вывел <dx-data-grid>.
    Данные для таблицы <dx-data-grid> беруться из сервиса employess.service.ts
    
    Всё заработало, но похоже без CSS стилей.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Employee, EmployeesService } from './employees.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  employees: Employee[] = [];
   title = 'my-third-dashboard';
+
+  constructor(service: EmployeesService) {
+    this.employees = service.getEmployees();
+  }
+
   helloWorld(): void {
     alert('Hello world!');
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { DxButtonModule } from 'devextreme-angular';
+import { DxButtonModule, DxDataGridModule } from 'devextreme-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     DxButtonModule,
+    DxDataGridModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
