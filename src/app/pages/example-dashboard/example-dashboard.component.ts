4ed69dccb0671a95a0ccfc62a5b353b1945b3be0
Tolik Dem <tolik.demianov@gmail.com>
2021-02-09 17:35:15 +0300

    Повесил два обработчика на Dashboard onBeforeRender и onDashboardInitialized
    Внутри этих обработчиков я так понимаю можно делать всякие полезности связанные с самой Dashboard.
    
    Например в onBeforeRender можно подключать (регистрировать) всякие расширения.
    
    Кроме этого в этом коммите попробовал использовать <dxo-viewer-api> и с обработчиком onItemWidgetcreated. Но пока не заработало.

--- src/app/pages/example-dashboard/example-dashboard.component.ts
+++ src/app/pages/example-dashboard/example-dashboard.component.ts
@@ -12,4 +12,23 @@ export class ExampleDashboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onDashboardInit(e: any): void {
+    console.log('Dashboard initialized', e);
+  }
+
+  onBeforeRender(e: any): void {
+    // const dashboardControl = e.component;
+    // dashboardControl.registerExtension(new ViewerApiExtension(dashboardControl));
+    // console.log('extensions', dashboardControl.extensions);
+    console.log('onBeforeRender');
+
+    // for (const property in dashboardControl.extensions) {
+    //   console.log(property, dashboardControl.extensions[property]);
+    // }
+  }
+
+  onItemWidgetCreated(item: any): void {
+    console.log('onItemWidgetCreated');
+  }
+
 }
